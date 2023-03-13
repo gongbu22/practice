3423749ea38945c10e932d3df77e4ba54b0e08de
gongbu22 <gongbu22@gmail.com>
2023-03-13 15:33:46 +0900

    230313Java

---
+++ HelloWorld/src/Hello2023.java
@@ -0,0 +1,14 @@
+
+public class Hello2023 {
+	
+	//main() 메소드에서 실행 시작
+	public static void main(String[] args) {
+		var price =200;
+		var name="kitae";
+		var pi =3.14;
+		
+		System.out.println("price="+(price+1000));
+		System.out.println("name="+name);
+		System.out.println("pi="+pi*10);
+	} 
+}
